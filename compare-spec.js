77d8ab1552959b192e8a2beffe9e1f8ea72c4baf
Sviatlana Kuraptsova <kuraptsova.s@gmail.com>
2017-10-11 16:36:25 +0300

    To handle dependencies: 1. install protractor, jasmine, etc. 2. run webdriver-manager update under ./node-modules/.bin
    otherwise globally installed webdriver-manager is being called
    
    To run test:
    1. Add script "test":"protractor conf.js"
    2. run "npm test" from command line

--- compare-spec.js
+++ compare-spec.js
@@ -3,9 +3,10 @@ var getMenuForCity = require("./getMenuForCity.js");
 
 describe('compare menu for 2 cities on Yandex page', function () {
     it('should compare menu items', function () {
+        browser.waitForAngularEnabled(false);
         browser.get('http://yandex.ru');
-        getMenuForCity.defineCity();
-        console.log(getMenuForCity);
+        //getMenuForCity.defineCity();
+        console.log(getMenuForCity.defineCity());
         browser.close();
     })
 })
