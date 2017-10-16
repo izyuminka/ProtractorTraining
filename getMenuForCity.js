969f5448fc2807d11dfac13f660118e0a105dd37
izyuminka <kuraptsova.s@gmail.com>
2017-10-17 00:45:19 +0300

    Array with all menu Items created

--- getMenuForCity.js
+++ getMenuForCity.js
@@ -1,22 +1,35 @@
-var defineCity = require("./defineCity.js");
+//var defineCity = require("./defineCity.js");
 
 exports.getMenuForCity = function () {
 
     //var promise = require('selenium-webdriver').promise;
-    var menuMoreElement = browser.findElements(By.xpath('//div[@class="home-tabs__more"]/child::div/descendant::*[@class="home-tabs__more-item"]'));
+
     //var menuElementsCount = menuMoreElement.size();
+    //var menuForCity = [];
 
-
+    var mArr = new Array();
 
     browser.element(By.xpath('//*[@data-statlog="tabs.more"]')).click();
     browser.wait(element(By.xpath('//div[@class="home-tabs__more"]')).isDisplayed());
+    var menuMoreElement =
+        browser.findElements(By.xpath('//div[@class="home-tabs__more"]/child::div/descendant::*[@class="home-tabs__more-item"]'))
+            .then(function (elements) {
+                elements.forEach(function (element) {
+                    element.getText().then(function(text){
+                        mArr.push(text);
+                        //var menuItem = String(text);
+                        //menuForCity.concat(text);
+                        //menuForCity = text;
+                        //console.log(menuForCity);
+
+                    });
+                })
+            }).then(function (value) { console.log(mArr) });
 
 
-    //TODO: change method to fetch all menu items
-    var menuItemsCount = menuMoreElement.length;
+    //console.log("A!" + menuMoreElement)
+        //var moreMenu = Array.from(menuMoreElement);
 
-
-    
     /*menuMoreElement.then(function (elements) {
         var menuForCity = elements.map(function (elem) {
             return elem.getInnerHtml();
@@ -38,5 +51,8 @@ exports.getMenuForCity = function () {
      })
     })
 });*/
-    console.log(menuMoreElement.length);
-        };
+    //console.log(moreMenu.length);
+
+
+};
+
